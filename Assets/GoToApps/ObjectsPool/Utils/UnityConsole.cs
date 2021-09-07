188cf5875b58b174195ed3a1190e7a4af296239f
n-fridman <n.fridman@goto-apps.com>
2021-09-07 08:25:40 +0300

    Update unity console util

--- Assets/GoToApps/ObjectsPool/Utils/UnityConsole.cs
+++ Assets/GoToApps/ObjectsPool/Utils/UnityConsole.cs
@@ -28,7 +28,7 @@ namespace GoToApps.ObjectsPool.Utils
         /// <param name="className">Class name</param>
         /// <param name="methodName">Method name</param>
         /// <param name="message">Message</param>
-        /// <param name="context">GameObject context.</param>>
+        /// <param name="context">GameObject context.</param>
         public static void PrintLog(string className, string methodName, string message, GameObject context)
         {
 #if UNITY_EDITOR
@@ -39,16 +39,33 @@ namespace GoToApps.ObjectsPool.Utils
         }
         
         /// <summary>
-        /// Draw log record to console.
+        /// Print log to console.
         /// </summary>
+        /// <param name="moduleName">Application module name</param>
         /// <param name="className">Class name</param>
         /// <param name="methodName">Method name</param>
-        /// <param name="message">Message</param>
-        /// <param name="context">GameObject context.</param>>
+        /// <param name="message">Message to console</param>
         public static void PrintLog(string moduleName,string className, string methodName, string message)
         {
 #if UNITY_EDITOR
             Debug.Log($"{{<b><color=white>{moduleName}</color></b>}} => [{className}] - (<color=yellow>{methodName}</color>) -> {message}");
+#else
+            Debug.Log($"{{GoTo-Apps}} => [{className}] - ({methodName}) -> {message}");
+#endif
+        }
+        
+        /// <summary>
+        /// Print log to console.
+        /// </summary>
+        /// <param name="moduleName">Application module name</param>
+        /// <param name="className">Class name</param>
+        /// <param name="methodName">Method name</param>
+        /// <param name="message">Message to console</param>
+        /// <param name="context">Game object context</param>
+        public static void PrintLog(string moduleName,string className, string methodName, string message, GameObject context)
+        {
+#if UNITY_EDITOR
+            Debug.Log($"{{<b><color=white>{moduleName}</color></b>}} => [{className}] - (<color=yellow>{methodName}</color>) -> {message}", context);
 #else
             Debug.Log($"{{GoTo-Apps}} => [{className}] - ({methodName}) -> {message}");
 #endif
