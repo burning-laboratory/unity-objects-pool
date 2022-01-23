e9daf9c3cb83b3bba52f1814d9d0daf362e5963c
n-fridman <n.fridman3116@gmail.com>
2022-01-23 09:11:09 +0300

    Update test utils

--- Assets/GoToApps/ObjectsPool/Tests/TestUtils.cs
+++ Assets/GoToApps/ObjectsPool/Tests/TestUtils.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace GoToApps.ObjectsPool.Tests
 {
@@ -29,6 +30,33 @@ namespace GoToApps.ObjectsPool.Tests
         public static SomePoolableItem CreatePoolableItemExtendedFromPoolableItem()
         {
             return new GameObject("Some Game Object").AddComponent<SomePoolableItem>();
+        }
+        
+        /// <summary>
+        /// Create a poolable item instance linked to game object.
+        /// </summary>
+        /// <returns>Instantiated poolable item.</returns>
+        public static SomePoolableItem CreatePoolableItemExtendedFromPoolableItem(string name)
+        {
+            return new GameObject(name).AddComponent<SomePoolableItem>();
+        }
+        
+        /// <summary>
+        /// Return random prefabs template.
+        /// </summary>
+        /// <returns>Prefabs template.</returns>
+        public static List<GameObject> GenerateRandomTemplate()
+        {
+            List<GameObject> gameObjects = new List<GameObject>();
+            int count = Random.Range(10, 50);
+
+            for (int i = 0; i < count; i++)
+            {
+                GameObject gameObject = CreateGameObjectExtendedFromPoolableItem($"Some Game Object-{i:00}");
+                gameObjects.Add(gameObject);
+            }
+            
+            return gameObjects;
         }
     }
 }
